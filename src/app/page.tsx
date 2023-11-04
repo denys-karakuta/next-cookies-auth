8eb90bcd3414007f747432fed78a630888a2edd6
Denys Karakuta <den456988@gmail.com>
2023-11-04 13:18:07 +0200

    feat: add HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import Link from 'next/link';
+
+import Button from '@/components/Button';
+
+const HomePage = () => {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            index page
+        <main className="w-full py-20 mx-auto lg:w-8/12 xl:w-5/12 md:py-32">
+            <h2 className="mt-2 text-lg font-medium text-center text-gray-800">
+                This is where you can do your authorization
+            </h2>
+
+            <div className="mt-4 flex flex-col items-center justify-center space-y-2 md:flex-row md:space-y-0 md:space-x-3">
+                <Link href="/signin">
+                    <Button text="Sign in" />
+                </Link>
+
+                <Link href="/signup">
+                    <Button text="Sign up" />
+                </Link>
+            </div>
         </main>
     );
-}
+};
+
+export default HomePage;
