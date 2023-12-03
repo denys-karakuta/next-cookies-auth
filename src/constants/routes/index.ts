8a65f0326ac32bbf44a9b983e22fa9a73c3a0549
Denys Karakuta <den456988@gmail.com>
2023-12-03 10:27:11 +0200

    feat: add PROTECTED_ROUTES

--- src/constants/routes/index.ts
+++ src/constants/routes/index.ts
@@ -1,8 +1,12 @@
-const ROUTES = {
+export const ROUTES = {
+    VERIFY_EMAIL: '/verify-email',
+    PROFILE: '/profile',
     SIGNIN: '/signin',
     SIGNUP: '/signup',
     LOGOUT: '/logout',
-    ME: '/me',
+    DEFAULT: '/',
 };
 
-export default ROUTES;
+export const PROTECTED_ROUTES = [ROUTES.VERIFY_EMAIL, ROUTES.PROFILE];
+
+export const PROTECTED_ROUTES_AFTER_SIGNIN = [ROUTES.SIGNIN, ROUTES.SIGNUP];
